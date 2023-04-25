428fb7f04be7fb4edc0f7601029e26c225ef5a86
noo-o <zxzx00202@naver.com>
2023-04-25 16:20:32 +0900

    접근제한자 및 상속

---
+++ ch07/sec02/Phone.java
@@ -0,0 +1,36 @@
+package ch07.sec02;
+
+public class Phone {
+	//필드 선언
+	public String model;
+	public String color;
+	
+	
+	
+	public Phone() {
+		System.out.println(" Phone");
+	}
+	
+	public Phone(String model, String color) {
+		super();
+		this.model = model;
+		this.color = color;
+		System.out.println("모델 컬러 실행");
+	}
+
+	public void bell() {
+		System.out.println("벨이 울립니다.");
+	}
+	
+	public void sendVoice(String message) {
+		System.out.println("자기 :"+ message);
+	}
+	
+	public void receiveVoice(String message) {
+		System.out.println("상대방 : "+ message);
+	}
+	
+	public void hangUp() {
+		System.out.println("전화를 끊습니다.");
+	}
+}
